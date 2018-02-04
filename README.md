f78a890c5a834c087a99ed17b841f4b0cda1d1ef
Anselm Brehme <development.anselm@breh.me>
2018-02-04 23:24:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+ore-deploy-maven-plugin
+====================
+
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/org.cubeengine.maven.plugins/ore-deploy-maven-plugin/badge.svg?style=flat)](https://maven-badges.herokuapp.com/maven-central/org.cubeengine.maven.plugins/ore-deploy-maven-plugin)
+[![Build Status](https://travis-ci.org/CubeEngine/ore-deploy-maven-plugin.svg?branch=master)](https://travis-ci.org/CubeEngine/ore-deploy-maven-plugin)
+
+Description
+-----------
+This plugin supports deploying sponge plugins to [Ore](https://ore.spongepowered.org)
---
+++ src/main/java/org/cubeengine/maven/plugins/oredeploy/mojo/OreDeployMojo.java
@@ -0,0 +1,66 @@
+/**
+ * The MIT License
+ * Copyright (c) 2018 CubeEngine
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.cubeengine.maven.plugins.oredeploy.mojo;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+@Mojo(name = "deploy", threadSafe = true)
+public class OreDeployMojo extends AbstractMojo
+{
+
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    private MavenProject project = null;
+
+    @Parameter(defaultValue = "${ore.deploy.pluginid}", required = true, readonly = true)
+    private String pluginId = null;
+
+    @Parameter(defaultValue = "${ore.deploy.version}", required = true, readonly = true)
+    private String version = null;
+
+    @Parameter(defaultValue = "${ore.deploy.apikey}", required = true, readonly = true)
+    private String apiKey = null;
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final void execute() throws MojoExecutionException, MojoFailureException
+    {
+        getLog().info("Uploading plugin " + pluginId + " ...");
+
+        /*
+        curl \
+  -F "apiKey=<apiKey>" \
+  -F "channel=test2" \
+  -F "pluginFile=@.jar" \
+  -F "pluginSig=@.jar.sig" \
+https://ore.spongepowered.org/api/projects/<pluginid>/versions/<version>
+        */
+    }
+}
