5370b26e65e724d9096bab21906326963b11f442
Phillip Schichtel <phillip@schich.tel>
2024-12-15 19:16:27 +0100

    fix some deprecation

--- src/main/java/org/cubeengine/maven/plugins/oredeploy/mojo/OreDeployMojo.java
+++ src/main/java/org/cubeengine/maven/plugins/oredeploy/mojo/OreDeployMojo.java
@@ -1,6 +1,6 @@
-/**
+/*
  * The MIT License
- * Copyright (c) 2018 CubeEngine
+ * Copyright © 2018 CubeEngine
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
